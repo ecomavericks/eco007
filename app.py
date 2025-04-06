565fba4dd0b44d7cf5d6ed10eb99eccaf0f2a044
ecomavericks <muthukumarcn4@gmail.com>
2025-04-06 18:12:52 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-# Load your HTML file
-with open("index.html", 'r', encoding='utf-8') as f:
-    html_content = f.read()
-
-# Display the HTML
-st.components.v1.html(html_content,height=600,scrolling=True)
